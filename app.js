c09f7339630c42f8c36dfbe5ecf54f67b839bd11
Brayan <bittolo.8bit@gmail.com>
2026-03-18 15:22:59 +0100

    Show route

--- app.js
+++ app.js
@@ -21,6 +21,43 @@ app.get("/movies", (req, res) => {
     });
 });
 
+// ROTTA SHOW
+app.get("/movies/:id", (req, res) => {
+    const movieId = req.params.id;
+
+    const sql = "SELECT * FROM movies WHERE id = ?";
+
+    db.query(sql, [movieId], (err, movieResults) => {
+        if (err) {
+            return res.status(500).json({
+                error: "Errore durante il recupero dei film",
+            });
+        }
+
+        if (movieRestults.length === 0) {
+            return res.status(404).json({
+                error: "Film non trovato",
+            });
+        }
+
+        const movie = movieRestults[0];
+
+        const reviewsSql = "SELECT * FROM reviews WHERE movie_id = ?";
+
+        db.query(sql, [movieId], (err, reviewsResults) => {
+            if (err) {
+                return res.status(500).json({
+                    error: "Errore durante il recupero delle recensioni",
+                });
+            }
+
+            movie.reviews = reviewsResults;
+
+            res.json(movie);
+        });
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server avviato sulla porta ${port}`);
 });
